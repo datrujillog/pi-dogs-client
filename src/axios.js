cb9031e3d86ae09b568ade03d717e540da6e5b1a
yego0210 <diegotrujillo0210@gmail.com>
2022-01-12 23:07:21 -0500

    App

--- src/axios.js
+++ src/axios.js
@@ -3,9 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API || "https://dogs-api-diego.herokuapp.com"
+    baseURL: process.env.REACT_APP_API || "https://pi-dogs-api.vercel.app"
 });
 
 export default instance;
 
 // http://localhost:3001
+// https://dogs-api-diego.herokuapp.com
